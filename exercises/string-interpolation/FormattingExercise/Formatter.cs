b3e6d707d6bfbaf0a198669d35902efc505ee5ec
Elian Bollue <elian.bollue@student.ucll.be>
2022-02-27 21:51:04 +0100

    Create Formatter.cs

---
+++ exercises/string-interpolation/FormattingExercise/Formatter.cs
@@ -0,0 +1,14 @@
+﻿namespace FormattingExercise
+{
+    public static class Formatter
+    {
+        public static string Greet(string name)
+        {
+            return $"Hello, {name}";
+        }
+        public static string FormatDate(int day, int month, int year)
+        {
+            return $"{day.ToString("D2")}/{month.ToString("D2")}/{year}";
+        }
+    }
+}
