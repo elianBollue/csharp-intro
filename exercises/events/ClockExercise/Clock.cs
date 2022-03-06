d650cfc930d7880bf97a0d4641267d45da3761ad
Elian Bollue <elian.bollue@student.ucll.be>
2022-03-06 23:16:50 +0100

    basics af

---
+++ exercises/events/ClockExercise/Clock.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClockExercise
+{
+    public class Clock
+    {
+        private int totalSeconds;
+        public Clock(ITimerService ts)
+        {
+            ts.Tick += OnTick;
+        }
+
+        public void OnTick()
+        {
+            totalSeconds++;
+
+            SecondPassed?.Invoke(totalSeconds);
+            HasMinutePassed();
+            HasHourPassed();
+            HasDayPassed();
+        }
+
+        private void HasMinutePassed()
+        {
+            if(totalSeconds % 60 == 0)
+            {
+                MinutePassed?.Invoke(totalSeconds / 60);
+            }
+        }
+        private void HasHourPassed()
+        {
+            if(totalSeconds % (60 * 60) == 0)
+            {
+                HourPassed?.Invoke(totalSeconds / (60 * 60));
+            }
+        }
+        private void HasDayPassed()
+        {
+            if (totalSeconds % (60 * 60 * 24) == 0)
+            {
+                DayPassed?.Invoke(totalSeconds / (60 * 60 * 24));
+            }
+        }
+
+        public Action<int> SecondPassed;
+        public Action<int> MinutePassed;
+        public Action<int> HourPassed;
+        public Action<int> DayPassed;
+    }
+}
---
+++ exercises/events/FileSystemExercise/ChangedFilesCounter.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace FileSystemExercise
+{
+    public class ChangedFilesCounter
+    {
+        private HashSet<string> changedFiles;
+        public ChangedFilesCounter(IFileSystem fs)
+        {
+            changedFiles = new HashSet<string>();
+            fs.FileCreated += OnFileModified;
+            fs.FileWritten += OnFileModified;
+            fs.FileDeleted += OnFileModified;
+        }
+
+        private void OnFileModified(string path)
+        {
+            changedFiles.Add(path);
+        }
+
+        public int ChangeCount {
+            get {
+                return changedFiles.Count;
+            }
+        }
+    }
+}
